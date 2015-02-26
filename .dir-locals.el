728b8dd7d7e0bb1b4b1d45b00b8b6fab058fa424
Steve Buzonas <steve@fancyguy.com>
2015-02-26 01:35:40 -0500

    initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((project-name . "FancyGuy Platform Installer Plugin")))
+ (php-mode . ((source-path . "src/")
+	      (source-namespace . "FancyGuy\\Composer\\Platform")
+	      (tests-path . "tests/")
+	      (tests-namespace . "FancyGuy\\Composer\\Platform\\Test"))))
