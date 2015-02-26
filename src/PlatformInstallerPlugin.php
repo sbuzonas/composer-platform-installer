8a8d9e0f46365c9e598697660ccc7f2eefe73a5c
Steve Buzonas <steve@fancyguy.com>
2015-02-26 01:39:51 -0500

    implement plugin interface

--- src/PlatformInstallerPlugin.php
+++ src/PlatformInstallerPlugin.php
@@ -14,7 +14,7 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class PlatformInstallerPlugin
+class PlatformInstallerPlugin implements PluginInterface
 {
     
     public function activate(Composer $composer, IOInterface $io)
